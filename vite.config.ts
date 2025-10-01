b7dee0cacbabec638a5bf0abb132984b0969138a
zhiyixn <26158700+flkGit@users.noreply.github.com>
2025-10-01 17:19:40 +0800

    修改配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: flkgit 26158700+flkGit@users.noreply.github.com
+ * @Date: 2025-09-27 21:33:18
+ * @LastEditors: flkgit 26158700+flkGit@users.noreply.github.com
+ * @LastEditTime: 2025-09-29 21:37:17
+ * @FilePath: /emos_home/vite.config.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by 26158700+flkGit@users.noreply.github.com, All Rights Reserved. 
+ */
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -48,7 +58,8 @@ export default defineConfig(({ mode }) => {
       port: 5173,
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:8000',
+          target: 'https://dev.emos.lol',
+          // target: 'http://127.0.0.1:8000',
           changeOrigin: true,
         },
       },
